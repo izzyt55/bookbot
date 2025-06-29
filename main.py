af00813f2925d2c1c224451eaf02e42a889c3da9
izzyt55 <iatemple55@gmail.com>
2025-06-29 12:13:27 -0400

    Added system arguments along with exception statements for errors

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from stats import count_number_of_words
 from stats import num_of_characters
 from stats import sorted_list
+import sys
+
+try:
+    sys_arg = sys.argv[1]
+except Exception as e:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,7 +15,7 @@ def get_book_text(filepath):
         return file_contents
     
 def main():
-    book_contents = get_book_text("books/frankenstein.txt")
+    book_contents = get_book_text(sys_arg)
     total_words = count_number_of_words(book_contents)
     total_characters = num_of_characters(book_contents)
 
@@ -29,11 +36,15 @@ def main():
 
     
     print("========= BOOKBOT =========\n"
-    "Analyzing book found at books/frankenstein.txt...\n" \
+    f"Analyzing book found at {sys_arg}...\n" \
         "----- Word Count -----")
     print(f"Found {total_words} total words")
     print("----- Character Count -----")
     print(report())
     print("========== END ==========")
 
-main()
+try:
+    main()
+except Exception as e:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
